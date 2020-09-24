d8c4bffd59cddc78bb60f94279182f6f42e99f55
Kouassi Kevin Kouadio <kevkouadio@gmil.com>
2020-09-24 10:42:53 -0400

    setup of handlebars files

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newdevoured = $(this).data("newdevoured");
+  
+      var newdevouredState = {
+        devoured: newdevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/cats/" + id, {
+        type: "PUT",
+        data: newdevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newdevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newCat = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/cats", {
+        type: "POST",
+        data: newCat
+      }).then(
+        function() {
+          console.log("created new cat");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-cat").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/cats/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted cat", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
